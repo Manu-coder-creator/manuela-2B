ec839216fc31c59a48d2694f89a3f23ee988b21f
Manu-coder-creator <manuela.cardoso.lima@escola.pr.gov.br>
2025-09-04 11:20:49 -0300

    .

---
+++ projeto2/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Biologia',
+    'Qual a organela responsável pela respiração celular?',
+    'A organela responsável pela respiração é a mitocôndria'
+)
+
+criaCartao (
+    'História',
+    'Quem foram os governantes da República da Espada no Brasil?',
+    'O presiente Deodoro da Fonseca e seu vice Floriano Peixoto'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é um aquífero?',
+    'Aquífero é uma reserva de água subterrânea'
+)
