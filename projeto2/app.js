280acbcd5c3640a4606bbe9c13ed774b32366614
Manu-coder-creator <manuela.cardoso.lima@escola.pr.gov.br>
2025-09-11 11:23:46 -0300

    ..

--- projeto2/app.js
+++ projeto2/app.js
@@ -1,9 +1,20 @@
 function criaCartao(categoria, pergunta, resposta) {
 
-   let container = document.getElementById ('container')
-   let cartao =document.createElement ('article')
-   cartao.className = 'cartao'
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+        <h3>Biologia</h3>
+        <div class="cartao__conteudo__pergunta">
+            <p>Qual a organela responsável pela respiração celular?</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+            <p>A organela responsável pela respiração é a mitocôndria</p>
+        </div>
+    </div>`
 
+    container.appendChild(cartao)
 }
 
 
